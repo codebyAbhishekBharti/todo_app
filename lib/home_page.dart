ebb02adf558ce14003bacada415676d5c66e012b
Abhishek Kumar Bharti <bhartiabhishek310@gmail.com>
2024-10-18 01:13:35 +0530

    added the feature that when a particular task list is clicked it will change color and store the value of current task list to get the tasklist name easily

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -14,14 +14,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final String userEmail = FirebaseAuth.instance.currentUser?.email ?? '';
+  String? selectedTask; // Variable to keep track of the selected task
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 22, 25, 22),
-        automaticallyImplyLeading: false, // This removes the back button
+        backgroundColor: Color.fromARGB(255, 25, 28, 30),
+        automaticallyImplyLeading: false,
         title: SizedBox(
-          height: kToolbarHeight, // This ensures the height matches the AppBar's height
+          height: kToolbarHeight,
           child: Stack(
             children: [
               Center(
@@ -44,10 +46,9 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   onPressed: () {
-                    print("Profile button is pressed");
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => ProfilePage()), // Navigate to ProfilePage
+                      MaterialPageRoute(builder: (context) => ProfilePage()),
                     );
                   },
                 ),
@@ -57,100 +58,127 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Container(
-        color: Color.fromARGB(255, 22, 25, 22),
-        child: Column(
-          children: [
-            Align(
-              alignment: Alignment.centerLeft,
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Container(
-                  padding: EdgeInsets.only(left: 0, right: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top:5.0),
-                        child: Container(
-                          padding: EdgeInsets.only(left: 15, right: 15),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(0.0),
-                                child: IconButton(
-                                  icon: Icon(Icons.star, color: Colors.white, size: 24),
-                                  onPressed: () {
-                                    // Handle button press
+        color: Color.fromARGB(255, 25, 28, 30),
+        child: Container(
+          margin: EdgeInsets.only(top: 15),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Container(
+                    padding: EdgeInsets.only(left: 0, right: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 5.0),
+                          child: Container(
+                            padding: EdgeInsets.only(left: 15, right: 15),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                GestureDetector(
+                                  onTap: () {
                                     print("Star button pressed");
-                                    print(userEmail);
+                                    print(selectedTask);
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(bottom: 15.0),
+                                    child: Icon(
+                                      Icons.star,
+                                      color: Colors.white,
+                                      size: 24,
+                                    ),
+                                  ),
+                                ),
+                                StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                                  stream: FirebaseFirestore.instance
+                                      .collection('users')
+                                      .doc(userEmail)
+                                      .snapshots(),
+                                  builder: (context, snapshot) {
+                                    if (snapshot.connectionState == ConnectionState.waiting) {
+                                      return Center(child: CircularProgressIndicator());
+                                    }
+
+                                    if (!snapshot.hasData || !snapshot.data!.exists) {
+                                      return Center(
+                                        child: Text(
+                                          '', // No tasks available
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      );
+                                    }
+
+                                    final taskList = snapshot.data!.data()?['tasks'];
+                                    if (taskList == null || taskList.isEmpty) {
+                                      return Center(
+                                        child: Text(
+                                          '', // No tasks available
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                      );
+                                    }
+
+                                    return SingleChildScrollView(
+                                      scrollDirection: Axis.horizontal,
+                                      child: Row(
+                                        children: taskList.map<Widget>((task) {
+                                          return GestureDetector(
+                                            onTap: () {
+                                              setState(() {
+                                                selectedTask = task; // Update selected task
+                                              });
+                                            },
+                                            child: Padding(
+                                              padding: EdgeInsets.only(left: 15, right: 15),
+                                              child: Column(
+                                                children: [
+                                                  Text(
+                                                    task.toString(),
+                                                    style: TextStyle(
+                                                      color: selectedTask == task
+                                                          ? Color.fromRGBO(26, 115, 232, 1)
+                                                          : Colors.white,
+                                                      fontSize: 14.0,
+                                                      fontWeight: FontWeight.w500,
+                                                    ),
+                                                  ),
+                                                  SizedBox(height: 12),
+                                                  // Conditionally render the Container only if the task is selected
+                                                  if (selectedTask == task)
+                                                    Container(
+                                                      width: 60,
+                                                      height: 5,
+                                                      decoration: BoxDecoration(
+                                                        color: Color.fromRGBO(26, 115, 232, 1), // Blue color when selected
+                                                        borderRadius: BorderRadius.only(
+                                                          topLeft: Radius.circular(10),
+                                                          topRight: Radius.circular(10),
+                                                        ),
+                                                      ),
+                                                    ),
+                                                ],
+                                              ),
+                                            ),
+                                          );
+
+                                        }).toList(),
+                                      ),
+                                    );
                                   },
                                 ),
-                              ),
-                              // Subtasks from the Firestore StreamBuilder
-                              StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                                stream: FirebaseFirestore.instance
-                                    .collection('users')
-                                    .doc(userEmail)
-                                    .snapshots(),
-                                builder: (context, snapshot) {
-                                  if (snapshot.connectionState == ConnectionState.waiting) {
-                                    return Center(child: CircularProgressIndicator());
-                                  }
-
-                                  if (!snapshot.hasData || !snapshot.data!.exists) {
-                                    return Center(
-                                      child: Text(
-                                        '', //no tasks available
-                                        style: TextStyle(color: Colors.white),
-                                      ),
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => NewList()),
                                     );
-                                  }
-
-                                  // Access the task field which is an array of task names
-                                  final taskList = snapshot.data!.data()?['tasks'];
-                                  if (taskList == null || taskList.isEmpty) {
-                                    return Center(
-                                      child: Text(
-                                        '', //No tasks available.
-                                        style: TextStyle(color: Colors.white),
-                                      ),
-                                    );
-                                  }
-
-                                  // Display tasks as a horizontal row
-                                  return SingleChildScrollView(
-                                    scrollDirection: Axis.horizontal,
-                                    child: Row(
-                                      children: taskList.map<Widget>((task) {
-                                        return Padding(
-                                          padding: EdgeInsets.only(left: 15, right: 15),
-                                          child: Text(
-                                            task.toString(),
-                                            style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 14.0,
-                                              fontWeight: FontWeight.w500,
-                                            ),
-                                          ),
-                                        );
-                                      }).toList(),
-                                    ),
-                                  );
-                                },
-                              ),
-                              GestureDetector(
-                                onTap: () {
-                                  // Handle tap
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(builder: (context) => NewList()), // Navigate to ProfilePage
-                                  );
-                                },
-                                child: Container(
-                                  padding: EdgeInsets.all(10),
+                                  },
                                   child: Padding(
-                                    padding: const EdgeInsets.only(left: 15.0),
+                                    padding: const EdgeInsets.only(bottom: 15.0),
                                     child: Text(
                                       "+ New list",
                                       style: TextStyle(
@@ -160,24 +188,24 @@ class _HomePageState extends State<HomePage> {
                                     ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              color: Colors.white,
-              child: SizedBox(
-                width: double.infinity,
-                height: 1,
+              Container(
+                color: Colors.white,
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 1,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
