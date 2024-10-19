89620046d85ec1f355ac59457fdda6061a543e04
Abhishek Kumar Bharti <bhartiabhishek310@gmail.com>
2024-10-19 12:29:10 +0530

    fixed the coloring issue of homepage and all dynamically switch between tasks and implemented functionality to delete task list

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,9 +2,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:todo_app/profile.dart';
-
 import 'new_list.dart';
-
+import 'package:todo_app/controller/task_handler.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -14,13 +13,14 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final String userEmail = FirebaseAuth.instance.currentUser?.email ?? '';
-  String? selectedTask; // Variable to keep track of the selected task
+  final taskHandler = TaskHandler();
+  String? selectedTask = "My Tasks"; // Variable to keep track of the selected task
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 25, 28, 30),
+        backgroundColor: Colors.grey[900],
         automaticallyImplyLeading: false,
         title: SizedBox(
           height: kToolbarHeight,
@@ -58,7 +58,7 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Container(
-        color: Color.fromARGB(255, 25, 28, 30),
+        color: Colors.grey[900],
         child: Container(
           margin: EdgeInsets.only(top: 15),
           child: Column(
@@ -142,7 +142,7 @@ class _HomePageState extends State<HomePage> {
                                                     task.toString(),
                                                     style: TextStyle(
                                                       color: selectedTask == task
-                                                          ? Color.fromRGBO(26, 115, 232, 1)
+                                                          ? Colors.blue
                                                           : Colors.white,
                                                       fontSize: 14.0,
                                                       fontWeight: FontWeight.w500,
@@ -153,9 +153,9 @@ class _HomePageState extends State<HomePage> {
                                                   if (selectedTask == task)
                                                     Container(
                                                       width: 60,
-                                                      height: 5,
+                                                      height: 3,
                                                       decoration: BoxDecoration(
-                                                        color: Color.fromRGBO(26, 115, 232, 1), // Blue color when selected
+                                                        color: Colors.blueAccent, // Blue color when selected
                                                         borderRadius: BorderRadius.only(
                                                           topLeft: Radius.circular(10),
                                                           topRight: Radius.circular(10),
@@ -166,7 +166,6 @@ class _HomePageState extends State<HomePage> {
                                               ),
                                             ),
                                           );
-
                                         }).toList(),
                                       ),
                                     );
@@ -180,7 +179,7 @@ class _HomePageState extends State<HomePage> {
                                     );
                                   },
                                   child: Padding(
-                                    padding: const EdgeInsets.only(bottom: 15.0),
+                                    padding: const EdgeInsets.only(bottom: 15.0, left: 15),
                                     child: Text(
                                       "+ New list",
                                       style: TextStyle(
@@ -206,10 +205,143 @@ class _HomePageState extends State<HomePage> {
                   height: 1,
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height - kToolbarHeight - 120,
+                  decoration: BoxDecoration(
+                    color: Colors.black87.withAlpha(180),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Align(
+                      alignment: Alignment.topLeft,
+                      child: Row(
+                        children: [
+                          Text(
+                            selectedTask ?? 'No task selected',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Spacer(), // This will push the following icons to the right
+                          Icon(
+                            Icons.swap_vert,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 10), // Optional spacing between icons
+                        IconButton(
+                          icon: Icon(
+                            Icons.more_vert,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {
+                            showModalBottomSheet(
+                              context: context,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(15),
+                                ),
+                              ),
+                              backgroundColor: Colors.grey[900]!.withAlpha(180),
+                              builder: (BuildContext context) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(15.0),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min, // Set the size to the minimum needed
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      ListTile(
+                                        title: Text(
+                                          'Rename list',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                        onTap: () {
+                                          // Handle rename list action
+                                          Navigator.pop(context); // Close the bottom sheet
+                                        },
+                                      ),
+                                      ListTile(
+                                        title: Text(
+                                          'Delete list',
+                                          style: (selectedTask=="My Tasks")?TextStyle(color: Colors.white.withAlpha(100)):TextStyle(color: Colors.white),
+                                        ),
+                                        // add description
+                                        subtitle: (selectedTask=="My Tasks")?Text(
+                                          "Default list can't be deleted",
+                                          style: (selectedTask=="My Tasks")?TextStyle(color: Colors.white.withAlpha(100),fontSize: 12):TextStyle(color: Colors.white,fontSize: 12),
+                                        ):null,
+                                        onTap: () async {
+                                          // Handle delete list action
+                                          if (selectedTask != "My Tasks") {
+                                            if(await taskHandler.deleteTask(selectedTask)){
+                                              setState(() {
+                                                selectedTask="My Tasks";
+                                              });
+                                              Navigator.pop(context);
+                                              print("Task deleted successfully main");
+
+                                              ScaffoldMessenger.of(context).showSnackBar(
+                                                SnackBar(
+                                                  content: Text('Task deleted successfully'),
+                                                  duration: Duration(seconds: 2),
+                                                  behavior: SnackBarBehavior.floating,
+                                                  margin: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+                                                ),
+                                              );
+                                            }
+                                            else{
+                                              print("Task deletion failed");
+                                            }
+                                          }
+                                        },
+                                      ),
+                                      ListTile(
+                                        title: Text(
+                                          'Delete all completed tasks',
+                                          style: TextStyle(color: Colors.white),
+                                        ),
+                                        onTap: () {
+                                          // Handle delete completed tasks action
+                                          Navigator.pop(context); // Close the bottom sheet
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                ),
+              )
             ],
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Navigator.push(
+          //   context,
+          //   MaterialPageRoute(builder: (context) => NewList()),
+          // );
+          print("add task button is pressed");
+        },
+        backgroundColor: Colors.blue.withAlpha(100),
+        child: Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
